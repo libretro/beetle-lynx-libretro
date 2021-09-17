fa28e6206925a9ea03af14edaee7ae28bfb78467
twinaphex <libretro@gmail.com>
2021-09-17 16:49:29 +0200

    Cleanups

--- mednafen/settings.cpp
+++ mednafen/settings.cpp
@@ -21,25 +21,6 @@
 #include <string>
 #include "settings.h"
 
-uint64 MDFN_GetSettingUI(const char *name)
-{
-
-   fprintf(stderr, "unhandled setting UI: %s\n", name);
-   return 0;
-}
-
-int64 MDFN_GetSettingI(const char *name)
-{
-   fprintf(stderr, "unhandled setting I: %s\n", name);
-   return 0;
-}
-
-double MDFN_GetSettingF(const char *name)
-{
-   fprintf(stderr, "unhandled setting F: %s\n", name);
-   return 0;
-}
-
 bool MDFN_GetSettingB(const char *name)
 {
    if (!strcmp("lynx.lowpass", name))
@@ -48,12 +29,5 @@ bool MDFN_GetSettingB(const char *name)
       return 0;
    if (!strcmp("cheats", name))
       return 0;
-   fprintf(stderr, "unhandled setting B: %s\n", name);
-   return 0;
-};
-
-std::string MDFN_GetSettingS(const char *name)
-{
-   fprintf(stderr, "unhandled setting S: %s\n", name);
    return 0;
 }
